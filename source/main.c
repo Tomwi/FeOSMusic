be4dbc0e9302a2bad5b80292e8cf1bc6e435b6ec
mtheall <pigman46@gmail.com>
2011-11-27 10:53:44 -0600

    add filebrowser

--- source/main.c
+++ source/main.c
@@ -1,13 +1,57 @@
 #include <feos.h>
 #include <stdio.h>
+#include <string.h>
+#include <filebrowser.h>
 #include "CodecInterface.h"
 #include "SndStream.h"
+
+#define TYPE_DIR(n) (n == DT_DIR ? 1 : 0)
+
+int filter(const struct dirent *dent) {
+	int len;
+
+	if(TYPE_DIR(dent->d_type)
+	&& strcmp(dent->d_name, ".") != 0)
+		return 1;
+
+	len = strlen(dent->d_name);
+	return stricmp(dent->d_name+len-4, ".ogg") == 0
+	    || stricmp(dent->d_name+len-4, ".m4a") == 0;
+}
+
+int compar(const struct dirent **dent1, const struct dirent **dent2) {
+	char isDir[2];
+
+	isDir[0] = TYPE_DIR((*dent1)->d_type);
+	isDir[1] = TYPE_DIR((*dent2)->d_type);
+
+	if(isDir[0] == isDir[1]) // sort by name
+		return stricmp((*dent1)->d_name, (*dent2)->d_name);
+	else
+		return isDir[1] - isDir[0]; // put directories first
+}
 
 CODEC_INTERFACE codec;
 int main(int argc, char ** argv)
 {
+	char *path;
+	char *type;
+	int   len;
 	initSoundStreamer();
-	startStream(&codec, argv[2], argv[1]);
+
+	path = pickFile("/", filter, compar);
+	if(path == NULL)
+		return 0;
+
+	len = strlen(path);
+	if(strcmp(path+len-4, ".ogg") == 0)
+		type = "ogg";
+	else if(strcmp(path+len-4, ".m4a") == 0)
+		type = "mp4";
+	else
+		return 0;
+
+	startStream(&codec, type, path);
 	while(1) {
 		FeOS_WaitForVBlank();
 		if(!updateStream(&codec)){
