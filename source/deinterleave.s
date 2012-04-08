3aa30800ec3df25599ee003c1f35d97a10a3ca68
mtheall <pigman46@gmail.com>
2012-04-08 01:13:10 -0500

    attempt to minimize memory wait states during writes in deinterleave

--- source/deinterleave.s
+++ source/deinterleave.s
@@ -1,5 +1,4 @@
 .equ BUF_BYTES, 8192*2
-.equ BUF_HW, 8192
 .global _deInterleave
 .hidden _deInterleave
 
@@ -8,7 +7,7 @@
 
 _deInterleave:
 	cmp r2, #4
-	bxlt lr		@ nothing to split
+	bxlt lr   @ nothing to split
 
 	bic r2, r2, #3
 	push {r4-r9}
@@ -17,29 +16,26 @@ _deInterleave:
 	mvn r8, #0
 	lsl r8,#16
 
-	.split:
-
+.split:
 	ldmia r0!,{r4-r7}
+
+	@ split left
+	bic r9, r4, r8
+	orr r9, r9, r5, lsl #16
+	@ split left
+	bic ip, r6, r8
+	orr ip, ip, r7, lsl #16
+	@ store left
+	stmia r1!,{r9,ip}
 
 	@ split right
 	and r9, r5, r8
 	orr r9, r9, r4, lsr #16
-	str r9,[r3],#4
-
-	@ split left
-	bic ip, r4, r8
-	orr ip, ip, r5, lsl #16
-	str ip,[r1],#4
-
 	@ split right
-	and r9, r7, r8
-	orr r9, r9, r6, lsr #16
-	str r9,[r3],#4
-
-	@ split left
-	bic ip, r6, r8
-	orr ip, ip, r7, lsl #16
-	str ip,[r1],#4
+	and ip, r7, r8
+	orr ip, ip, r6, lsr #16
+	@ store right
+	stmia r3!,{r9,ip}
 
 	subs r2,#4
 	bne .split
